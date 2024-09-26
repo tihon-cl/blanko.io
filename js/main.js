4866f209a48f31eddd18e2802d4beee319b1007b
tihon-cl <tikhon.gizhinskiy@yandex.ru>
2024-09-26 23:04:11 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("burger").addEventListener("click", function(){
-    document.getElementById("menu-box").classList.add("open")
-})
-
-document.getElementById("close").addEventListener("click", function(){
-    document.getElementById("menu-box").classList.remove("open")
-})
-
